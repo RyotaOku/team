5366439c37a5aa181c46fa475d170aca5e366255
RyotaOku <ryota1991o@gmail.com>
2025-02-04 14:53:14 +0900

    main

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,13 +1,12 @@
-'use client'
+'use client';
 
-// Firebase SDK の各機能をインポート
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getDatabase } from 'firebase/database';
 
-// Firebase プロジェクトの設定オブジェクト
+// Firebaseプロジェクトの設定
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -15,17 +14,13 @@ const firebaseConfig = {
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 };
 
-// Firebase アプリを初期化し、プロジェクトの設定を適用
+// Firebase初期化
 const app = initializeApp(firebaseConfig);
 
-// 各Firebaseサービスの初期化
-const db = getFirestore(app);
-const auth = getAuth(app);
-const storage = getStorage(app);
-const database = getDatabase(app);
-
-// 他のファイルから各サービスを使用できるようにエクスポート
-export { db, auth, storage, database };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const database = getDatabase(app);
